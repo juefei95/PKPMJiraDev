79c272f51c0cb2c5259b204ed1735bdc1132250f
shijianxin <shijianxin@cabrtech.com>
2021-05-12 20:52:33 +0800

    添加产品延期的不一致项，修复bug

--- JiraReport_dev.js
+++ JiraReport_dev.js
@@ -17,15 +17,15 @@ class Inconsistency{
 }
 
 class DeveloperDelay extends Inconsistency {
-    constructor(jiraId, title, responsible, developer, planDate){
-        super(responsible);
+    constructor(jiraId, title, designer, developer, planDate){
+        super(designer);
         this.jiraId = jiraId;
         this.title = title;
         this.developer = developer;
         this.planDate = planDate;
     }
     static check(i){
-        if (i.designer != 'Empty Field' && i.status==="研发中" && i.program_plan_commit_date < today && i.doc_plan_commit_date.getTime() !== theModel.initStartDate.getTime()) {
+        if (i.designer !== 'Empty Field' && i.status==="研发中" && i.program_plan_commit_date < new Date().setHours(0,0,0) && i.doc_plan_commit_date.getTime() !== theModel.initStartDate.getTime()) {
             return new DeveloperDelay(i.recid, i.title, i.designer, i.developer, i.program_plan_commit_date);
         }
         return undefined;
@@ -39,20 +39,47 @@ class DeveloperDelay extends Inconsistency {
     }
 }
 
+class DesignerDelay extends Inconsistency {
+    constructor(jiraId, title, designer, planDate){
+        super(designer);
+        this.jiraId = jiraId;
+        this.title = title;
+        this.planDate = planDate;
+    }
+    static check(i){
+        if (i.designer !== 'Empty Field' && i.status==="需求待设计" && i.doc_plan_commit_date < new Date().setHours(0,0,0) && i.doc_plan_commit_date.getTime() !== theModel.initStartDate.getTime()) {
+            return new DesignerDelay(i.recid, i.title, i.designer, i.doc_plan_commit_date);
+        }
+        return undefined;
+    }
+    toHtml(){
+        
+        var html = `
+        <b>产品设计逾期：</b><a href="https://jira.pkpm.cn/browse/${this.jiraId}"  target="_blank">${this.jiraId}</a> 产品 ${this.developer}，计划日期${this.planDate.toISOString().substring(0, 10)}，标题为${this.title}
+        `;
+        return html;
+    }
+}
+
 var theApp = {
 
     initFrame : function(){
         let qaInconsistency = {};
+        let checkFuncs = [
+            DeveloperDelay.check,
+            DesignerDelay.check,
+        ];
         theModel.issues.forEach(i => {
-            let ret = DeveloperDelay.check(i);
-            if (ret){
-                if(ret.Responsible() in qaInconsistency){
-                    qaInconsistency[ret.Responsible()].push(ret);
-                }else{
-                    qaInconsistency[ret.Responsible()] = [ret];
+            checkFuncs.forEach(f => {
+                let ret = f(i);
+                if (ret){
+                    if(ret.Responsible() in qaInconsistency){
+                        qaInconsistency[ret.Responsible()].push(ret);
+                    }else{
+                        qaInconsistency[ret.Responsible()] = [ret];
+                    }
                 }
-                
-            }
+            });
         });
         let ul = document.createElement("ul");
         for (const [k, v] of Object.entries(qaInconsistency)){
