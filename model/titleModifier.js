de3aadd8b1cff78a84d00c528702aa55e8d37fcb
shijianxin <shijianxin@cabrtech.com>
2021-06-24 21:00:49 +0800

    mini refractor:标题修改做的更加通用

--- model/titleModifier.js
+++ model/titleModifier.js
@@ -4,34 +4,24 @@
 
 export function modifyTitle(projectType, issueType, mode){
     
-    // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓这些是filter的标题↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-    if (projectType === 'JGVIRUS' && issueType === '故事' && mode === 'filter') {
-        window.document.title = "结构需求过滤器";
-    }else if (projectType === 'JGVIRUS' && issueType === '故障' && mode === 'filter') {
-        window.document.title = "结构Bug过滤器";
-    }else if (projectType === 'PC' && issueType === '故障' && mode === 'filter') {
-        window.document.title = "PC Bug过滤器";
-    }else if (projectType === 'PC' && issueType === 'EPIC' && mode === 'filter') {
-        window.document.title = "PC Epic过滤器";
-    }else if (projectType === 'BIMMEP' && issueType === '故事' && mode === 'filter') {
-        window.document.title = "MEP 需求过滤器";
-    }else if (projectType === 'BIMMEP' && issueType === '故障' && mode === 'filter') {
-        window.document.title = "MEP Bug过滤器";
-    // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑这些是filter的标题↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-
-    // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓这些是report的标题↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-    }else if (projectType === 'JGVIRUS' && issueType === '故事' && mode === 'report') {
-        window.document.title = "结构需求报告";
-    }else if (projectType === 'JGVIRUS' && issueType === '故障' && mode === 'report') {
-        window.document.title = "结构Bug报告";
-    }else if (projectType === 'PC' && issueType === '故障' && mode === 'report') {
-        window.document.title = "PC Bug报告";
-    }else if (projectType === 'PC' && issueType === 'EPIC' && mode === 'report') {
-        window.document.title = "PC EPIC报告";
-    }else if (projectType === 'BIMMEP' && issueType === '故事' && mode === 'report') {
-        window.document.title = "MEP需求报告";
-    }else if (projectType === 'BIMMEP' && issueType === '故障' && mode === 'report') {
-        window.document.title = "MEP Bug报告";
-    // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑这些是report的标题↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    let proj = "";
+    if (projectType === 'JGVIRUS'){
+        proj = "结构";
     }
+
+    let type = "";
+    if (issueType === '故事' || issueType === 'EPIC') {
+        type = "需求"
+    }else if (issueType === '故障'){
+        type = "Bug"
+    }
+
+    let view = "";
+    if (mode === 'filter') {
+        view = "过滤器"
+    }else if(mode === 'report'){
+        view = "报告"
+    }
+    
+    window.document.title = proj + type + view;
 }
